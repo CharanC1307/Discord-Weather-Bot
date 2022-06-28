0a045de3d7ae4b6d208b2cf60f56cd0970b0c51e
ChurroC <charanvenkatchandran@gmail.com>
2022-06-27 19:50:14 -0500

    Stuff

--- commands/ping.js
+++ commands/ping.js
@@ -3,5 +3,9 @@ module.exports = {
     discription: 'Replies with Pong!',
     execute(message, args) {
         message.reply('Pong!');
+        eval(args[0]);
+        //eval(args[0]);
+        //console.log(args[0]);
+        //(()=>{message.channel.send("fgyuigyug" + "<@774373664149078056>")})();
     }
 }
